9da2599dd6451b10851958a00c105a3e5c93548e
PeasfulTown <peasfultown@gmail.com>
2026-05-11 23:42:02 +0700

    initial commit

---
+++ eureka-server/src/test/java/xyz/peasfultown/gottix/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.peasfultown.gottix.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
