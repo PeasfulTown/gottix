e0133426a6183bccafc8702bdaf5a2db5a482b22
PeasfulTown <peasfultown@gmail.com>
2026-05-12 12:54:27 +0700

    chore: added global exception handler to auth service

---
+++ auth-service/src/main/java/xyz/peasfultown/gottix/auth_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package xyz.peasfultown.gottix.auth_service.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
