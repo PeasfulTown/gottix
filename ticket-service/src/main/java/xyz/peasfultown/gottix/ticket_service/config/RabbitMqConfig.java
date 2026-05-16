053bfdb7f5bcf6950f0b13999429ca717c060e6d
PeasfulTown <peasfultown@gmail.com>
2026-05-17 05:15:49 +0700

    added outbox poller and rabbitmq publisher

--- ticket-service/src/main/java/xyz/peasfultown/gottix/ticket_service/config/RabbitMqConfig.java
+++ ticket-service/src/main/java/xyz/peasfultown/gottix/ticket_service/config/RabbitMqConfig.java
@@ -1,12 +1,92 @@
 package xyz.peasfultown.gottix.ticket_service.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
     public static String exchange;
-    public static String ticket_update_queue;
-    public static String ticket_update_routingKey;
 
+    public static String ticket_change_queue;
+    public static String ticket_change_routingKey;
+
+    public static String ticketComment_change_queue;
+    public static String ticketComment_change_routingKey;
+
+    // ============================================================
+    // DECLARATIONS
+    // ============================================================
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Queue ticket_change_queue() {
+        return new Queue(ticket_change_queue);
+    }
+
+    @Bean
+    public Binding ticketChangeQueue_binding(TopicExchange exchange, Queue ticket_change_queue) {
+        return BindingBuilder.bind(ticket_change_queue).to(exchange).with(ticket_change_routingKey);
+    }
+
+    @Bean
+    public Queue ticketComment_change_queue() {
+        return new Queue(ticketComment_change_queue);
+    }
+
+    @Bean
+    public Binding ticketCommentChangeQueue_binding(TopicExchange exchange, Queue ticketComment_change_queue) {
+        return BindingBuilder.bind(ticketComment_change_queue).to(exchange).with(ticketComment_change_routingKey);
+    }
+
+    // ============================================================
+    // CONFIGURATION
+    // ============================================================
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setChannelTransacted(true);
+        return rabbitTemplate;
+    }
+
+    // ============================================================
+    // SETTERS
+    // ============================================================
+
+    @Value("${rabbitmq.exchange.main}")
+    public void setExchange(String exchange) {
+        RabbitMqConfig.exchange = exchange;
+    }
+
+    @Value("${rabbitmq.queue.ticket-change}")
+    public void setTicket_change_queue(String ticket_change_queue) {
+        RabbitMqConfig.ticket_change_queue = ticket_change_queue;
+    }
+
+    @Value("${rabbitmq.routing-key.ticket-change}")
+    public void setTicket_change_routingKey(String ticket_change_routingKey) {
+        RabbitMqConfig.ticket_change_routingKey = ticket_change_routingKey;
+    }
+
+    @Value("${rabbitmq.queue.ticket-comment-change}")
+    public void setTicketComment_change_queue(String ticketComment_change_queue) {
+        RabbitMqConfig.ticketComment_change_queue = ticketComment_change_queue;
+    }
+
+    @Value("${rabbitmq.routing-key.ticket-comment-change}")
+    public void setTicketComment_change_routingKey(String ticketComment_change_routingKey) {
+        RabbitMqConfig.ticketComment_change_routingKey = ticketComment_change_routingKey;
+    }
 
 }
--- ticket-service/src/main/resources/db/migration/V1__init.sql
+++ ticket-service/src/main/resources/db/migration/V1__init.sql
@@ -66,3 +66,11 @@ CREATE TABLE IF NOT EXISTS outbox (
     created_at  TIMESTAMPTZ NOT NULL,
     updated_at  TIMESTAMPTZ NOT NULL
 );
+
+CREATE INDEX idx_outbox_ticket_status_pending
+ON outbox (entity_type, status, created_at ASC)
+WHERE entity_type = 'TICKET' AND status = 'PENDING';
+
+CREATE INDEX idx_outbox_comment_status_pending
+ON outbox (entity_type, status, created_at ASC)
+WHERE entity_type = 'COMMENT' AND status = 'PENDING';
