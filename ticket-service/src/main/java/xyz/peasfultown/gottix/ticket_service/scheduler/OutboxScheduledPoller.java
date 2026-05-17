a2b9ed2371ffcd6e7c30df5dac7581a7765ad929
PeasfulTown <peasfultown@gmail.com>
2026-05-18 03:57:05 +0700

    use appropriate outbox service methods for the scheduled poller methods

--- ticket-service/src/main/java/xyz/peasfultown/gottix/ticket_service/scheduler/OutboxScheduledPoller.java
+++ ticket-service/src/main/java/xyz/peasfultown/gottix/ticket_service/scheduler/OutboxScheduledPoller.java
@@ -1,24 +1,12 @@
 package xyz.peasfultown.gottix.ticket_service.scheduler;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageBuilder;
-import org.springframework.amqp.core.MessageProperties;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import xyz.peasfultown.gottix.ticket_service.config.RabbitMqConfig;
-import xyz.peasfultown.gottix.ticket_service.entity.OutboxEntity;
-import xyz.peasfultown.gottix.ticket_service.entity.OutboxStatus;
-import xyz.peasfultown.gottix.ticket_service.entity.TicketEntity;
-import xyz.peasfultown.gottix.ticket_service.repository.OutboxRepository;
-
-import java.util.List;
+import xyz.peasfultown.gottix.ticket_service.service.OutboxService;
 
 @Slf4j
 @Component
@@ -26,36 +14,14 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class OutboxScheduledPoller {
-    private static final String TYPE_ID_HEADER = "__TypeId__";
-    private static final String EVENT_TYPE_HEADER = "x-event-type";
-
-    private final OutboxRepository outboxRepo;
-    private final RabbitTemplate rabbitTemplate;
-    private final ObjectMapper objMapper;
+    private final OutboxService outboxService;
 
     @Scheduled(
             initialDelay = 5000,
             fixedDelay = 5000
     )
     public void sendOutboxPendingTickets() {
-        List<OutboxEntity> tickets = outboxRepo.findOldestPendingTickets(10);
-
-        for (OutboxEntity oe : tickets) {
-            try {
-                Message m = MessageBuilder.withBody(objMapper.writeValueAsBytes(oe.getPayload()))
-                        .setHeader(TYPE_ID_HEADER, "Ticket")
-                        .setHeader(EVENT_TYPE_HEADER, oe.getEventType().name())
-                        .setContentType(MessageProperties.CONTENT_TYPE_JSON)
-                        .build();
-
-                log.info("sending ticket change event: {}", m);
-                rabbitTemplate.send(RabbitMqConfig.exchange, RabbitMqConfig.ticket_change_routingKey, m);
-                oe.setStatus(OutboxStatus.PROCESSED);
-            } catch (JsonProcessingException e) {
-                log.error("unable to send ticket change event", e);
-                throw new RuntimeException("unable to send ticket change event", e);
-            }
-        }
+        outboxService.sendOutboxPendingTickets();
     }
 
     @Scheduled(
@@ -63,27 +29,7 @@ public class OutboxScheduledPoller {
             fixedDelay = 5000
     )
     public void sendOutboxPendingComments() {
-        List<OutboxEntity> comments = outboxRepo.findOldestPendingComments(10);
-
-        if (comments.isEmpty())
-            return;
-
-        for (OutboxEntity oe : comments) {
-            try {
-                Message m = MessageBuilder.withBody(objMapper.writeValueAsBytes(oe.getPayload()))
-                        .setHeader(TYPE_ID_HEADER, "Comment")
-                        .setHeader(EVENT_TYPE_HEADER, oe.getEventType().name())
-                        .setContentType(MessageProperties.CONTENT_TYPE_JSON)
-                        .build();
-
-                log.info("sending comment change event: {}", m);
-                rabbitTemplate.send(RabbitMqConfig.exchange, RabbitMqConfig.ticketComment_change_routingKey, m);
-                oe.setStatus(OutboxStatus.PROCESSED);
-            } catch (JsonProcessingException e) {
-                log.error("unable to send ticket comment change event", e);
-                throw new RuntimeException("unable to send ticket comment change event", e);
-            }
-        }
+        outboxService.sendOutboxPendingComments();
     }
 
     @Scheduled(
@@ -91,12 +37,6 @@ public class OutboxScheduledPoller {
             fixedDelay = 10000
     )
     public void cleanProcessedEntities() {
-        List<OutboxEntity> entities = outboxRepo.findAllProcessed(10);
-
-        if (entities.isEmpty())
-            return;
-
-        log.info("cleaning up processed outbox entities");
-        outboxRepo.deleteAll(entities);
+        outboxService.cleanupProcessedItems();
     }
 }
--- ticket-service/src/main/java/xyz/peasfultown/gottix/ticket_service/service/OutboxServiceImpl.java
+++ ticket-service/src/main/java/xyz/peasfultown/gottix/ticket_service/service/OutboxServiceImpl.java
@@ -1,24 +1,46 @@
 package xyz.peasfultown.gottix.ticket_service.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import xyz.peasfultown.gottix.ticket_service.config.RabbitMqConfig;
 import xyz.peasfultown.gottix.ticket_service.dto.OutboxComment;
 import xyz.peasfultown.gottix.ticket_service.dto.OutboxTicket;
 import xyz.peasfultown.gottix.ticket_service.entity.*;
 import xyz.peasfultown.gottix.ticket_service.repository.OutboxRepository;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(propagation = Propagation.MANDATORY)
 public class OutboxServiceImpl implements OutboxService {
+    private static final String TYPE_ID_HEADER = "__TypeId__";
+    private static final String EVENT_TYPE_HEADER = "x-event-type";
+
     private final OutboxRepository outboxRepo;
+    private final RabbitTemplate rabbitTemplate;
     private final ObjectMapper objMapper;
+
+    @Override
+    public List<OutboxEntity> getOldestPendingTickets(
+            int batchSize) {
+        return outboxRepo.findOldestPendingTickets(batchSize);
+    }
+
+    @Override
+    public void updateOutboxEventStatus(OutboxEntity oe, OutboxStatus status) {
+        oe.setStatus(status);
+    }
 
     @Override
     public void saveTicketToOutbox(TicketEntity te, EventType type) {
@@ -60,4 +82,63 @@ public class OutboxServiceImpl implements OutboxService {
         log.info("saved comment to outbox: {}", oe);
     }
 
+    @Override
+    public void sendOutboxPendingTickets() {
+        List<OutboxEntity> tickets = outboxRepo.findOldestPendingTickets(10);
+
+        for (OutboxEntity oe : tickets) {
+            try {
+                Message m = MessageBuilder.withBody(objMapper.writeValueAsBytes(oe.getPayload()))
+                        .setHeader(TYPE_ID_HEADER, "Ticket")
+                        .setHeader(EVENT_TYPE_HEADER, oe.getEventType().name())
+                        .setContentType(MessageProperties.CONTENT_TYPE_JSON)
+                        .build();
+
+                log.info("sending ticket change event: {}", m);
+                rabbitTemplate.send(RabbitMqConfig.exchange, RabbitMqConfig.ticket_change_routingKey, m);
+                oe.setStatus(OutboxStatus.PROCESSED);
+            } catch (
+                    JsonProcessingException e) {
+                log.error("unable to send ticket change event", e);
+                throw new RuntimeException("unable to send ticket change event", e);
+            }
+        }
+
+    }
+
+    @Override
+    public void sendOutboxPendingComments() {
+        List<OutboxEntity> comments = outboxRepo.findOldestPendingComments(10);
+
+        if (comments.isEmpty())
+            return;
+
+        for (OutboxEntity oe : comments) {
+            try {
+                Message m = MessageBuilder.withBody(objMapper.writeValueAsBytes(oe.getPayload()))
+                        .setHeader(TYPE_ID_HEADER, "Comment")
+                        .setHeader(EVENT_TYPE_HEADER, oe.getEventType().name())
+                        .setContentType(MessageProperties.CONTENT_TYPE_JSON)
+                        .build();
+
+                log.info("sending comment change event: {}", m);
+                rabbitTemplate.send(RabbitMqConfig.exchange, RabbitMqConfig.ticketComment_change_routingKey, m);
+                oe.setStatus(OutboxStatus.PROCESSED);
+            } catch (JsonProcessingException e) {
+                log.error("unable to send ticket comment change event", e);
+                throw new RuntimeException("unable to send ticket comment change event", e);
+            }
+        }
+    }
+
+    @Override
+    public void cleanupProcessedItems() {
+        List<OutboxEntity> entities = outboxRepo.findAllProcessed(10);
+
+        if (entities.isEmpty())
+            return;
+
+        log.info("cleaning up processed outbox entities");
+        outboxRepo.deleteAll(entities);
+    }
 }
